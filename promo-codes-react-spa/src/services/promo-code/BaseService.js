ac40e61da711c79fa0be300f9dfa8f1609897a6a
olugt <30696063+olugt@users.noreply.github.com>
2021-04-26 21:00:28 +0100

    Enhanced exception filtering of infrastructure web API exceptionsl

--- promo-codes-react-spa/src/services/promo-code/BaseService.js
+++ promo-codes-react-spa/src/services/promo-code/BaseService.js
@@ -1,7 +1,11 @@
 import ErrorModel from "../../common/models/ErrorModel";
-import { ApiException, ErrorResponseModel, PromoCodesAspNetCoreWebApiClient } from "../../infrastructure/web-api/PromoCodesAspNetCoreWebApiClient";
+import { ApiException, ObjectErrorResponseModel, PromoCodesAspNetCoreWebApiClient, ValidationErrorResponseModel } from "../../infrastructure/web-api/PromoCodesAspNetCoreWebApiClient";
 
 export default class BaseService {
+    /**
+     * Constructs with an identity token.
+     * @param {String} token The identity token, to be passed by derived classes that have it.
+     */
     constructor(token) {
         this.client = new PromoCodesAspNetCoreWebApiClient(process.env.REACT_APP_PROMO_CODES_WEB_API_BASE_URL);
         if (token) {
@@ -11,10 +15,54 @@ export default class BaseService {
         this.clientDefaultVersion = process.env.REACT_APP_PROMO_CODES_WEB_API_DEFAULT_VERSION;
     }
 
+    /**
+     * Transforms errors by derived classes.
+     * @param {*} error Any exception or error to be transformed to ErrorModel.
+     * @returns ErrorModel transformed from error.
+     */
     static TransformError(error) {
-        let apiException = new ApiException();
-        Object.assign(apiException, error);
-        let responseModel = new ErrorResponseModel(JSON.parse(apiException.response));
-        return new ErrorModel(responseModel.message, responseModel.data);
+        // console.log(JSON.stringify(error));
+        let errorModel = new ErrorModel();
+
+        if (error instanceof ApiException) {
+            let apiException = new ApiException();
+            Object.assign(apiException, error);
+
+            if (apiException.status && apiException.response) {
+                /**
+                 * @type {{message: string, data: {}}}
+                 */
+                let allError = JSON.parse(error.response);
+                console.log("AAAAAA");
+                console.log(allError);
+
+                let allErrorProps = Object.getOwnPropertyNames(allError);
+
+                if (!allErrorProps.some(a => Object.getOwnPropertyNames(new ValidationErrorResponseModel()).find(b => b !== a))) {
+                    let valdErrResModel = new ValidationErrorResponseModel();
+                    Object.assign(valdErrResModel, allError);
+
+                    errorModel.message = valdErrResModel.message;
+                    errorModel.data = valdErrResModel.data;
+                } else if (!allErrorProps.some(a => Object.getOwnPropertyNames(new ObjectErrorResponseModel()).find(b => b !== a))) {
+                    let objErrResModel = new ObjectErrorResponseModel();
+                    Object.assign(objErrResModel, allError);
+
+                    errorModel.message = objErrResModel.message;
+                    errorModel.data = objErrResModel.data;
+                } else {
+                    errorModel.message = "Undocumented specific error occurred.";
+                    errorModel.data = allError;
+                }
+            } else {
+                errorModel.message = apiException.message;
+                errorModel.data = error;
+            }
+        } else {
+            errorModel = new ErrorModel("Undocumented error occurred.", error);
+        }
+
+        // console.log(errorModel);
+        return errorModel;
     }
 };
