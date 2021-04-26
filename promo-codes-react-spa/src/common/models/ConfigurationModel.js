7f9fe63a51ca0c7cc67e0fb6e3efb138083cac59
olugt <30696063+olugt@users.noreply.github.com>
2021-04-26 20:59:29 +0100

    Added configuration for default page items limit and created configuration model for custom configurations.

---
+++ promo-codes-react-spa/src/common/models/ConfigurationModel.js
@@ -0,0 +1,16 @@
+/**
+ * Model for managing custom configurations, available to components.
+ */
+export default class ConfigurationModel {
+    constructor() {
+        
+    }
+
+    /**
+     * Get the default page items limit.
+     * @returns {String}
+     */
+    getDefaultPageItemsLimit() {
+        return process.env.REACT_APP_DEFAULT_PAGE_ITEMS_LIMIT;
+    }
+};
