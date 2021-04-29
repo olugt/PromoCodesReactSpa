fa845b43e9672a21452177425a852bb80dc7369e
olugt <30696063+olugt@users.noreply.github.com>
2021-04-29 02:00:23 +0100

    Completed feature about copying to clipboard, managing errors (e.g. validation), etc. about services.

--- promo-codes-react-spa/src/components/services/Services.js
+++ promo-codes-react-spa/src/components/services/Services.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import searchServices from "../../services/searchServices";
 import activateBonus from '../../services/activateBonus'
 import ServiceModel from "../../common/models/ServiceModel";
@@ -6,10 +6,14 @@ import getServices from "../../services/getServices";
 import { getInputValue } from "../../common/logic/formLogic";
 import useConfigurationContext from "../../hooks/contexts/useConfigurationContext";
 import useTokenContext from "../../hooks/contexts/useTokenContext";
+import { ERROR_CODES } from "../../common/constants/ErrorCodesConstants";
+import { MAGIC_STRINGS } from "../../common/constants/MagicStringsConstants";
+import ServiceIdToggleModel from "../../common/models/toggles/ServiceIdToggleModel";
 
 function Services(props) {
 
     const [forcedRenderToggle, setForcedRenderToggle] = useState(false);
+
     const [pagination, setPagination] = useState({ page: 1, limit: useConfigurationContext().state.getDefaultPageItemsLimit() });
     const [serviceNameSnippet, setServiceNameSnippet] = useState("");
 
@@ -17,33 +21,64 @@ function Services(props) {
      * @type {[services: ServiceModel[], setServices: (_: ServiceModel) => undefined]}
      */
     const [services, setServices] = useState([]);
-    const { state: tokenDetailState, setState: _setTokenDetailState } = useTokenContext();
+    const { state: tokenContextState, setState: _setTokenContextState } = useTokenContext();
+
+    const [serviceIdToActivate, setServiceIdToActivate] = useState(new ServiceIdToggleModel(false, null));
+
+    //
+
+    const serviceIdToActivateCallback = useCallback(
+        /**
+         * 
+         * @param {HTMLInputElement} element 
+         */
+        (element) => {
+            if (element != null) {
+                if ((MAGIC_STRINGS.promoInputIdPrefix + serviceIdToActivate.value) === element.id) {
+                    const textToCopy = element.value;
+                    if (textToCopy) {
+                        element.select();
+                        if (document.execCommand) {
+                            document.execCommand("copy");
+                        } else if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+                            navigator.clipboard.writeText(textToCopy);
+                        }
+                    }
+                }
+            }
+        },
+        [serviceIdToActivate],
+    )
 
     //
 
     useEffect(() => {
-        if (serviceNameSnippet) {
-            if (serviceNameSnippet.length >= 2) {
-                searchServices(
-                    tokenDetailState,
-                    serviceNameSnippet,
-                    pagination.page,
-                    pagination.limit,
-                    setServices,
-                    (error) => {
-                        console.log("Error at search services aaaaaa.")
-                        alert(error.message);
-                    });
-            }
-        } else {
-            getServices(
-                tokenDetailState,
+        if (serviceNameSnippet && serviceNameSnippet.length >= 2) {
+            searchServices(
+                tokenContextState,
+                serviceNameSnippet,
                 pagination.page,
                 pagination.limit,
                 setServices,
                 (error) => {
-                    console.log("Error at get services oooo.")
-                    console.log(error);
+                    if (error.code === ERROR_CODES.notFoundError) {
+                        setServices([]);
+                    } else {
+                        alert(error.message);
+                    }
+                });
+        } else {
+            getServices(
+                tokenContextState,
+                pagination.page,
+                pagination.limit,
+                setServices,
+                (error) => {
+                    if (error.code === ERROR_CODES.notFoundError) {
+                        setServices([]);
+                    } else {
+                        alert(error.message);
+                    }
                 })
         }
         return () => {
@@ -57,7 +92,6 @@ function Services(props) {
         <div className="container">
             <div className="row">
                 <div className="col-12"><h1>Services</h1></div>
-                <div>Token: {tokenDetailState?.token}</div>
             </div>
             <div className="row">
                 <div className="col-12">
@@ -66,7 +100,7 @@ function Services(props) {
                             <label>FILTER</label>
                             <input onChange={(e) => {
                                 setServiceNameSnippet(e.target.value);
-                            }} value={serviceNameSnippet} className="form-control" type="text" />
+                            }} value={serviceNameSnippet} className="form-control" type="text" title="Type in service name or snippet, at least 2 characters." />
                         </div>
                         <div className="col-2">
                             <label className="">*</label>
@@ -85,7 +119,7 @@ function Services(props) {
                             <form onSubmit={(e) => {
                                 e.preventDefault();
                                 activateBonus(
-                                    tokenDetailState,
+                                    tokenContextState,
                                     getInputValue(e.target, "promoCode"),
                                     getInputValue(e.target, "serviceId"),
                                     (serviceId) => {
@@ -94,8 +128,23 @@ function Services(props) {
                                         setForcedRenderToggle(!forcedRenderToggle);
                                     },
                                     (error) => {
-                                        console.log("Error at activate bonus eeeeeee.")
-                                        console.log(error);
+                                        let errorMessage = error.message;
+                                        if (error.code === ERROR_CODES.validationError) {
+                                            /**
+                                             * @type {{property: String[]}}
+                                             */
+                                            let errorData = error.data;
+                                            Object.keys(errorData).forEach((property) => {
+                                                /**
+                                                 * @type {String[]}
+                                                 */
+                                                let propertyValue = errorData[property];
+                                                propertyValue.forEach((value, index, array) => {
+                                                    errorMessage += `\n${value}`;
+                                                });
+                                            });
+                                        }
+                                        alert(errorMessage);
                                     });
                             }} className="container" key={item.id}>
                                 <div className="jumbotron row">
@@ -104,7 +153,15 @@ function Services(props) {
                                         {item.name}
                                     </h2>
                                     <div className="col-4">
-                                        <input type="text" className="form-control" name="promoCode" />
+                                        <input required id={MAGIC_STRINGS.promoInputIdPrefix + item.id} ref={serviceIdToActivateCallback} type="text" className="form-control" name="promoCode" placeholder="Enter valid promo code." />
+                                    </div>
+                                    <div className="col-2">
+                                        <button onClick={(e) => {
+                                            e.preventDefault();
+
+                                            setServiceIdToActivate(new ServiceIdToggleModel(!serviceIdToActivate.isOn, e.target.value));
+
+                                        }} value={item.id} className="form-control btn btn-secondary">Copy</button>
                                     </div>
                                     <div className="col-4">
                                         {item.isActivated ?
