22967d1f03d4ce208f04c7ac6d4495b0c861a47b
olugt <30696063+olugt@users.noreply.github.com>
2021-04-29 01:21:14 +0100

    Added configuration about whether or not the promo code web API infrastructure dependecy should be faked.

--- promo-codes-react-spa/src/common/models/contexts/ConfigurationContextModel.js
+++ promo-codes-react-spa/src/common/models/contexts/ConfigurationContextModel.js
@@ -1,9 +1,14 @@
+import { ERROR_CODES } from "../../constants/ErrorCodesConstants";
+import { convertFromBooleanString } from "../../logic/configurationLogic";
+import ErrorModel from "../ErrorModel";
+import ContextModelBase from "./ContextModelBase";
+
 /**
  * Model for managing custom configurations, available to components.
  */
-export default class ConfigurationContextModel {
+export default class ConfigurationContextModel extends ContextModelBase {
     constructor() {
-        
+        super();
     }
 
     /**
@@ -12,5 +17,14 @@ export default class ConfigurationContextModel {
      */
     getDefaultPageItemsLimit() {
         return process.env.REACT_APP_DEFAULT_PAGE_ITEMS_LIMIT;
+    }
+
+    /**
+     * Check if mocking of promo codes infrastructure is enabled.
+     * @returns {Boolean}
+     */
+    shouldMockPromoCodesInfrastructure() {
+        const configValue = process.env.REACT_APP_SHOULD_MOCK_PROMO_CODES_INFRASTRUCTURE;
+        return convertFromBooleanString(configValue);
     }
 };
