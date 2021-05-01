13adcb1b29a41d0cd7afa1515bb88e4440e3c2f7
olugt <30696063+olugt@users.noreply.github.com>
2021-05-01 17:45:36 +0100

    Added more configuration functions about pagination and loading other newly added configuration data (i.e. refer to privious commit).

--- promo-codes-react-spa/src/common/models/contexts/ConfigurationContextModel.js
+++ promo-codes-react-spa/src/common/models/contexts/ConfigurationContextModel.js
@@ -15,16 +15,48 @@ export default class ConfigurationContextModel extends ContextModelBase {
      * Get the default page items limit.
      * @returns {String}
      */
-    getDefaultPageItemsLimit() {
-        return process.env.REACT_APP_DEFAULT_PAGE_ITEMS_LIMIT;
+    getDefaultPageItemsLimitFactor() {
+        return process.env.REACT_APP_DEFAULT_PAGE_ITEMS_LIMIT_FACTOR;
+    }
+
+    /**
+     * Get minimum number of characters to type into the services search field before data is loaded.
+     * @returns {Number}
+     */
+    getMinimumCharactersLimitForServicesSearchField() {
+        return Number.parseInt(process.env.REACT_APP_MINIMUM_CHARACTERS_LIMIT_SERVICES_SEARCH);
     }
 
     /**
      * Check if mocking of promo codes infrastructure is enabled.
-     * @returns {Boolean}
+     * @returns 
      */
-    shouldFakePromoCodesInfrastructure() {
-        const configValue = process.env.REACT_APP_SHOULD_FAKE_PROMO_CODES_INFRASTRUCTURE;
+    shouldMockPromoCodesInfrastructure() {
+        const configValue = process.env.REACT_APP_SHOULD_MOCK_PROMO_CODES_INFRASTRUCTURE;
         return convertFromBooleanString(configValue);
+    }
+
+    /**
+     * 
+     * @returns {String} Base URL of the promo codes web API.
+     */
+    getPromoCodesWebApiBaseUrl() {
+        return process.env.REACT_APP_PROMO_CODES_WEB_API_BASE_URL;
+    }
+
+    /**
+     * 
+     * @returns {String} Default version to use for promo codes web API.
+     */
+    getPromoCodesWebApiDefaultVersion() {
+        return process.env.REACT_APP_PROMO_CODES_WEB_API_DEFAULT_VERSION;
+    }
+
+    /**
+     * 
+     * @returns {Number} Fake JWT expiration duration in minutes.
+     */
+    getFakeJwtExpirationDurationMinutes() {
+        return Number.parseInt(process.env.REACT_APP_FAKE_JWT_EXPIRATION_DURATION_MINUTES);
     }
 };
